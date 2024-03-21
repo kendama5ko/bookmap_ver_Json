26fa233aa2e9161dd16cf2f85b6b4db8eb38a570
kendama5ko <hukurahagix@yahoo.co.jp>
2024-03-21 18:28:10 +0900

    [修正] 1.指定したカラム以外をクリックしてもカレンダーが出てくる問題 2.少しドラッグされただけでカレンダーがでてこなかった問題への対処として、mouseClickではなくmouseReleasedされた時にカレンダーを表示

--- src/controller/CalendarDialog.java
+++ src/controller/CalendarDialog.java
@@ -2,6 +2,7 @@ package controller;
 
 import javax.swing.*;
 import javax.swing.event.CellEditorListener;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableColumn;
 
@@ -17,21 +18,27 @@ import java.util.EventObject;
 
 import com.toedter.calendar.JCalendar;
 
+import dao.JsonDAO;
+
 public class CalendarDialog {
 
-    private JCalendar calendar;
+    private JsonDAO jdao;
     private JDialog dialog;
+    private JCalendar calendar;
     private JLabel editingLabel;
+    private JTable progressDataTable;
 
-    public CalendarDialog(JTable table) {
-        initializeUI(table);
+    public CalendarDialog(JTable progressDataTable) {
+        initializeUI(progressDataTable);
     }
 
-    private void initializeUI(JTable table) {
+    private void initializeUI(JTable progressDataTable) {
+        this.jdao = new JsonDAO();
+        this.dialog = new JDialog();
+        this.calendar = new JCalendar();
+        this.editingLabel = new JLabel();
+        this.progressDataTable = progressDataTable;
 
-        dialog = new JDialog();
-        calendar = new JCalendar();
-        editingLabel = new JLabel();
         // ダイアログの設定
         dialog.setTitle("Select Date");
         dialog.setSize(300, 300);
@@ -41,7 +48,7 @@ public class CalendarDialog {
         // ダイアログを閉じた時にセルの編集も終了
         dialog.addWindowListener(new WindowAdapter() {
             public void windowClosed(WindowEvent e) {
-                table.removeEditor();
+                progressDataTable.removeEditor();
             }
         });
     }
@@ -49,20 +56,29 @@ public class CalendarDialog {
     /*
      * カレンダーの日付が選択された時の挙動
      */
-    public void onDateSelected(JTable table) {
+    public void onDateSelected(DefaultTableModel progressModel) {
         calendar.getDayChooser().addPropertyChangeListener("day", evt -> {
+            this.jdao = new JsonDAO();
+
             // 日付を取得し、フォーマットの形式に変換
             Date selectedDate = calendar.getDate();
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy MM/dd");
             String formattedDate = dateFormat.format(selectedDate);
-
+            
             // 選択された日付を JTable のセルにセット
-            int selectedRow = table.getSelectedRow();
-            int selectedColumn = table.getSelectedColumn();
-            table.setValueAt(formattedDate, selectedRow, selectedColumn);
+            int selectedRow = progressDataTable.getSelectedRow();
+            int selectedColumn = progressDataTable.getSelectedColumn();
+            
+            // 本のIDを取得
+            String bookID = (String) progressDataTable.getValueAt(selectedRow, 3);
+            Long createdAtLong = (Long) progressDataTable.getValueAt(selectedRow, 2);
 
+            //progressDataTable.setValueAt(formattedDate, selectedRow, selectedColumn);
+            progressModel.setValueAt(formattedDate, selectedRow, selectedColumn);
+
+            jdao.editProgressDataFromCalendar(bookID, createdAtLong, formattedDate);
             // [重要] 編集状態を解除する（これをしないと編集状態のままになり、日付が入らない）
-            table.removeEditor();
+            progressDataTable.removeEditor();
             dialog.dispose();
         });
     }
@@ -70,74 +86,97 @@ public class CalendarDialog {
     /*
      * dateカラムを選択時のセルエディターの設定
      */
-    public void onDateColumnSelected(JTable table) {
+    public TableColumn onDateColumnSelected() {
         // テーブルのdateカラムのセルエディタを設定（1ではなく変数で受け取る方が保守性が高い？)
-        TableColumn dateColumn = table.getColumnModel().getColumn(1);
+        TableColumn dateColumn = progressDataTable.getColumnModel().getColumn(1);
 
         dateColumn.setCellEditor(new TableCellEditor() {
-            @Override
-            public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
-                    int column) {
+			@Override
+			public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
+					int column) {
+				// セルが編集中の場合のみ表示する
+				if (isSelected) {
+					// 日付を編集中にセルに表示されるテキスト
+					editingLabel.setText("日付を選択");
+					editingLabel.setHorizontalAlignment(JLabel.CENTER);
+					return editingLabel;
 
-                table.addMouseListener(new MouseAdapter() {
-                    public void mouseClicked(MouseEvent e) {
-                        // クリックされた少し左上にdailogを表示
-                        int x = e.getXOnScreen() - 350;
-                        int y = e.getYOnScreen() - 200;
-                        
-                        // 画面上部や左にdialogが見切れないようにする
-                        if (e.getXOnScreen() < 350) {
-                            x = 0;
-                        }
-                        if (e.getYOnScreen() < 200) {
-                            y = 0;
-                        }
-                        dialog.setLocation(x, y);
-                        if (!dialog.isVisible()) {
-                            dialog.setVisible(true);
-                        }
-                    }
-                });
-                // 日付を編集中にセルに表示されるテキスト
-                editingLabel.setText("日付を選択");
-                editingLabel.setHorizontalAlignment(JLabel.CENTER);
-                return editingLabel;
-            }
+					// 編集中でない場合は何も表示しない
+				} else {
+					return null;
+				}
+			}
 
-            @Override
-            public Object getCellEditorValue() {
-                return null;
-            }
+			@Override
+			public Object getCellEditorValue() {
+				return null;
+			}
 
-            @Override
-            public boolean isCellEditable(EventObject anEvent) {
-                return true;
-            }
+			@Override
+			public boolean isCellEditable(EventObject anEvent) {
+				return true;
+			}
 
-            @Override
-            public boolean shouldSelectCell(EventObject anEvent) {
-                return true;
-            }
+			@Override
+			public boolean shouldSelectCell(EventObject anEvent) {
+				return true;
+			}
 
-            @Override
-            public boolean stopCellEditing() {
-                dialog.dispose(); // ダイアログを閉じる
-                table.removeEditor();
-                return true;
-            }
+			@Override
+			public boolean stopCellEditing() {
+				dialog.dispose(); // ダイアログを閉じる
+				progressDataTable.removeEditor();
+				return true;
+			}
 
-            @Override
-            public void cancelCellEditing() {
-                dialog.dispose(); // ダイアログを閉じる
-            }
+			@Override
+			public void cancelCellEditing() {
+				dialog.dispose(); // ダイアログを閉じる
+			}
 
-            @Override
-            public void addCellEditorListener(CellEditorListener l) {
-            }
+			@Override
+			public void addCellEditorListener(CellEditorListener l) {
+			}
 
-            @Override
-            public void removeCellEditorListener(CellEditorListener l) {
-            }
-        });
+			@Override
+			public void removeCellEditorListener(CellEditorListener l) {
+			}
+		});
+
+        return dateColumn;
     }
+public void openCalendarSetting() {
+    progressDataTable.addMouseListener(new MouseAdapter() {
+        public void mouseReleased(MouseEvent e) {
+            int selectedRow = progressDataTable.rowAtPoint(e.getPoint());
+            int selectedColumn = progressDataTable.columnAtPoint(e.getPoint());
+            // カラム1がクリックされたときだけ処理を実行
+            if (selectedColumn == 1) {
+                // クリックされたセルを編集状態にする
+                progressDataTable.editCellAt(selectedRow, selectedColumn);
+                // クリックされた少し左上にdailogを表示
+                int x = e.getXOnScreen() + 5;
+                int y = e.getYOnScreen() - 305;
+                // GraphicsDevice gd = frame.getGraphicsConfiguration().getDevice();
+                // int width = gd.getDisplayMode().getWidth();
+                // int height = gd.getDisplayMode().getHeight();
+
+                // 画面上部や左にdialogが見切れないようにする
+                if (e.getXOnScreen() < 350) {
+                    x = 0;
+                }
+                if (e.getYOnScreen() < 305) {
+                    y = 0;
+                }
+                dialog.setLocation(x, y);
+                dialog.setVisible(true);
+
+            } else if (selectedColumn != 1) {
+                dialog.dispose();
+            }
+        }
+    });
+}
+
+
 }
